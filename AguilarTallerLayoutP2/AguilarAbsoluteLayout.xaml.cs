ed555757e904613a5a14425cd623cadcdd0c64b1
Jordy-Aguilar <jeay022@gmail.com>
2024-11-15 09:04:20 -0500

    Agregue todas las paginas con su boton de navegacion

--- AguilarTallerLayoutP2/AguilarAbsoluteLayout.xaml.cs
+++ AguilarTallerLayoutP2/AguilarAbsoluteLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class AguilarAbsoluteLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void BotonAbsoluteLayout_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new AguilarGridLayout());
+    }
 }
--- AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
+++ AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class AguilarFlexLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void BotonFlexLayout_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new AguilarAbsoluteLayout());
+    }
 }
