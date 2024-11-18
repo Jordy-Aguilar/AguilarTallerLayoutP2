579f760802afe5e26c35e8ee16ba876a80e46515
Jordy-Aguilar <jeay022@gmail.com>
2024-11-18 08:38:05 -0500

    Agregue informacion e imagenes en pagina AbsoluteLayout

--- AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
+++ AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
@@ -6,7 +6,10 @@ public partial class AguilarFlexLayout : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AguilarAbsoluteLayout());
+    }
     private void BotonFlexLayout_Clicked(object sender, EventArgs e)
     {
 		Navigation.PushAsync(new AguilarAbsoluteLayout());
