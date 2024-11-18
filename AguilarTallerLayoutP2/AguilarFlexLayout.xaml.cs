9388f1823281d08b0ba5ee53f6bfd90798784a4b
Jordy Aguilar <jeay022@gmail.com>
2024-11-18 18:27:15 -0500

    Actualice pagina FlexLayout

--- AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
+++ AguilarTallerLayoutP2/AguilarFlexLayout.xaml.cs
@@ -6,10 +6,7 @@ public partial class AguilarFlexLayout : ContentPage
 	{
 		InitializeComponent();
 	}
-    private async void OnImageTapped(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new AguilarAbsoluteLayout());
-    }
+    
     private void BotonFlexLayout_Clicked(object sender, EventArgs e)
     {
 		Navigation.PushAsync(new AguilarAbsoluteLayout());
